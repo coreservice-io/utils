dd01ea70a70ab1339d72608f5c274c32ccb0cf2a
goodboy3 <zhangzhengbo@hotmail.com>
2022-01-02 15:40:46 +0800

    commit

--- rand_util/gen.go
+++ rand_util/gen.go
@@ -3,25 +3,41 @@ package rand_util
 import (
 	"math/rand"
 	"time"
+	"unsafe"
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 
-func GenRandStr(n int) string {
-	if n <= 0 {
-		return ""
-	}
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
-	}
-	return string(b)
-}
+const letterBytes_len = len(letterBytes)
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
 
 func init() {
 	ResetSeed()
 }
-
 func ResetSeed() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+var randsrc = rand.NewSource(time.Now().UnixNano())
+
+func GenRandStr(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, randsrc.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = randsrc.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < letterBytes_len {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return *(*string)(unsafe.Pointer(&b))
 }
---
+++ rand_util/gen_test.go
@@ -0,0 +1,13 @@
+package rand_util
+
+import (
+	"testing"
+)
+
+func Benchmark_randStr(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		GenRandStr(10)
+	}
+}
