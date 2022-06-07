1524cc601b72ac51ab0fdc6bfd5962be4c2201ef
leo <leo@leos-MacBook-Pro.local>
2022-06-07 11:11:13 +0800

    update

--- path_util/path.go
+++ path_util/path.go
@@ -19,13 +19,19 @@ func AbsPathExist(path string) (bool, error) {
 	return false, err
 }
 
-//return a abs folder/file path ,
+// return a abs folder/file path ,
 // if no such file or folder then err is not nil
-func SmartExistPath(relpath string) (string, error) {
+func SmartExistPath(abs_or_rel_path string) (string, error) {
 
-	exist, _ := AbsPathExist(ExE_Path(relpath))
+	//check if abs path exist
+	abs_exist, _ := AbsPathExist(abs_or_rel_path)
+	if abs_exist {
+		return abs_or_rel_path, nil
+	}
+
+	exist, _ := AbsPathExist(ExE_Path(abs_or_rel_path))
 	if exist {
-		return ExE_Path(relpath), nil
+		return ExE_Path(abs_or_rel_path), nil
 	}
 
 	//if user run from root as working directory
@@ -33,7 +39,7 @@ func SmartExistPath(relpath string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	w_path := filepath.Join(currDir, relpath)
+	w_path := filepath.Join(currDir, abs_or_rel_path)
 	w_p_exist, _ := AbsPathExist(w_path)
 	if !w_p_exist {
 		return "", errors.New("path not exist")
